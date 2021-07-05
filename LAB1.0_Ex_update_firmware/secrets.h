cd040f12e6bc99103c83afa2b96d40ee1036c186
GridsDev <83551564+GridsNodeMCU@users.noreply.github.com>
2021-07-05 13:40:00 +0700

    update

---
+++ LAB1.0_Ex_update_firmware/secrets.h
@@ -0,0 +1,3 @@
+// Insert your WiFi secrets here:
+#define SECRET_SSID "" // Your network SSID   
+#define SECRET_PASS "" // Your network password
--- LAB1/secrets.h
+++
@@ -1,3 +0,0 @@
-// Insert your WiFi secrets here:
-#define SECRET_SSID "domnick-WiFi" // Your network SSID   
-#define SECRET_PASS "domnickhunterrl" // Your network password
